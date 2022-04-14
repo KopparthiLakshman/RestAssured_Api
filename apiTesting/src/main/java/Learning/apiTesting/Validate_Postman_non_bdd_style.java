c63fb95ec950c5f1e2e8dc56a5034b41db64047c
mohan <mohan@LAPTOP-9TPAEKJH>
2022-04-14 21:22:34 +0530

    APIUpdate

--- apiTesting/src/main/java/Learning/apiTesting/Validate_Postman_non_bdd_style.java
+++ apiTesting/src/main/java/Learning/apiTesting/Validate_Postman_non_bdd_style.java
@@ -1,25 +1,77 @@
 package Learning.apiTesting;
 
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.builder.ResponseSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+
 import static io.restassured.RestAssured.*;
 
+
+// this will create workspace and delete the workspace also
 public class Validate_Postman_non_bdd_style {
+
+	@BeforeClass
+	public void beforeClass(){
+
+		RequestSpecBuilder rsb = new RequestSpecBuilder();
+					rsb.setBaseUri("https://api.postman.com").
+					addHeader("x-api-key", "PMAK-62570a58c7b6e906537591c7-c9d9366692510b68dbdfac426c0627c0a2").
+					setContentType(ContentType.JSON).
+					log(LogDetail.ALL);
+
+		RestAssured.requestSpecification = rsb.build();
+
+		ResponseSpecBuilder responseSB = new ResponseSpecBuilder();
+		responseSB.expectStatusCode(200).
+		expectContentType(ContentType.JSON).
+		log(LogDetail.ALL);
+		
+		RestAssured.responseSpecification = responseSB.build();
+
+		
+	}
+
 	@Test
 	public void Validate_Postman_non_bdd_style_1() {
-		String payload = "{    \"workspace\": \r\n"
+		String payload = "{\"workspace\": \r\n"
 				+ "        {\r\n"
-				+ "            \"name\": \"Workspace_Post_eclipse\",\r\n"
+				+ "            \"name\": \"Work2_eclipse2\",\r\n"
 				+ "            \"type\": \"personal\",\r\n"
 				+ "            \"description\" : \"Creating Workspace with api request\"\r\n"
 				+ "        }\r\n"
 				+ "}";
+
+		System.out.println(payload);
+	
+		
+		Response res = 	with().
+								body(payload).post("/workspaces").
+						then().
+								extract().response();
+		
+		String id =JsonPath.from(res.asString()).get("workspace.id");
+		
+		System.out.println(id+"  :: id of the workspace");
+		
+// Deletion of created workspace
+		delete_workspace_Created_In_Above_method(id);
+	}
+	
+	
+	public void delete_workspace_Created_In_Above_method(String id) {
+
+		String deleteWorkspace=id;
 		with().
-				baseUri(payload).post("/workspaces").
+			  delete("/workspaces/"+deleteWorkspace).
 		then().
 				log().all();
-					
-		
-		
-		
 	}
+
 }
--- apiTesting/src/main/java/Learning/apiTesting/log_if_only_error.java
+++ apiTesting/src/main/java/Learning/apiTesting/log_if_only_error.java
@@ -2,18 +2,31 @@ package Learning.apiTesting;
 
 import org.testng.annotations.Test;
 
+import io.restassured.config.LogConfig;
+
 import static io.restassured.RestAssured.*;
 
 public class log_if_only_error {
 
 	
-	@Test
+//	@Test
 	public void log_if_only_error_method() {
 		given().
-				baseUri("")
+				baseUri("https://api.postman.com").
+				header("x-api-key", "PMAK-62570a58c7b6e906537591c7-c9d9366692510b68dbdfac426c0627c0a2").
+//				log().ifValidationFails().
+//				config(config.logConfig(LogConfig.logConfig().enableLoggingOfRequestAndResponseIfValidationFails())).
+				config(config.logConfig(LogConfig.logConfig().blacklistHeader("x-api-key", "Accept"))).
+				log().all().
+		when().
+				get("/workspaces").
+		then().
+//				log().ifError().
+				log().ifError().
+				assertThat().
+				statusCode(200)
 		
-		.when()
-		.then();
+		;
 	}
 	
 }
